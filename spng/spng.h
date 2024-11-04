4bf18e39f1d5bec4c2393dde910a2d9fde3d4d64
Pai Peng <sipaipv6@gmail.com>
2024-11-04 11:11:22 +0800

    update to objective-c project

---
+++ spng/spng.h
@@ -0,0 +1,12 @@
+//
+//  spng.h
+//  spng
+//
+//  Created by Pai Peng on 04.11.24.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface spng : NSObject
+
+@end
---
+++ spng/spng.m
@@ -0,0 +1,12 @@
+//
+//  spng.m
+//  spng
+//
+//  Created by Pai Peng on 04.11.24.
+//
+
+#import "spng.h"
+
+@implementation spng
+
+@end
